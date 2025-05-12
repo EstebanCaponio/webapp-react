cf0336234ca6604fa9165dcd4484dec6785c7066
EstebanCaponio <estebancaponio@gmail.com>
2025-05-12 16:16:09 +0200

    feat: created routes, layout and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import Films from "./pages/Films"
+import DefaultLayout from "./layout/DefaultLayout"
 
 
 function App() {
@@ -6,7 +9,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path='/films' element={<Films />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
